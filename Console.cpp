b9c261f47a7f7af2ffa96b77a8c70cfb3aeb9e7a
Ji Ho Nam <wlgh0123456@gmail.com>
2023-09-02 00:57:06 -0400

    Add files via upload

--- Console.cpp
+++ Console.cpp
@@ -14,19 +14,19 @@ void GotoXY(int _x, int _y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-//void SetTitle(const char* L_szConsoleName)
-//{
-//	// char* 형식을 wchar_t* 형식으로 변환
-//	int len = MultiByteToWideChar(CP_UTF8, 0, L_szConsoleName, -1, NULL, 0);
-//	wchar_t* wideString = new wchar_t[len];
-//	MultiByteToWideChar(CP_UTF8, 0, L_szConsoleName, -1, wideString, len);
-//
-//	// 변환된 문자열을 SetConsoleTitle에 전달
-//	SetConsoleTitle(wideString);
-//
-//	// 동적 할당된 메모리 해제
-//	delete[] wideString;
-//}
+void SetTitle(const char* L_szConsoleName)
+{
+
+	int len = MultiByteToWideChar(CP_UTF8, 0, L_szConsoleName, -1, NULL, 0);
+	wchar_t* wideString = new wchar_t[len];
+	MultiByteToWideChar(CP_UTF8, 0, L_szConsoleName, -1, wideString, len);
+
+
+	SetConsoleTitle(wideString);
+
+
+	delete[] wideString;
+}
 
 void SetColor(unsigned char _BgColor, unsigned char _TextColor)
 {
